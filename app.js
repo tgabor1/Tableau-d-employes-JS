64cb56c07f8a995e37431db891fffba71f93b865
tgabor1 <t.gabor@tutanota.com>
2022-06-28 12:32:47 +0200

    Début tableau d'employés

---
+++ app.js
@@ -0,0 +1,9 @@
+let employee1 = new Employee('Gabor', 'Théodore', '2017', 'Développeur', '2000€', 'Développement')
+let employee2 = new Employee('Nom2', 'Prénom2', '2018', 'Poste1', '1600€', 'Service1')
+let employee3 = new Employee('Nom3', 'Prénom2', '2019', 'Poste2', '1700€', 'Service2')
+let employee4 = new Employee('Nom4', 'Prénom3', '2020', 'Poste3', '1800€', 'Service3')
+let employee5 = new Employee('Nom5', 'Prénom4', '2021', 'Poste4', '1900€', 'Service4')
+
+let tableEmployees = [employee1, employee2, employee3, employee4, employee5]
+
+let 
---
+++ class.js
@@ -0,0 +1,13 @@
+class Employee {
+    constructor(name, firstname, hiringdate, position, salary, service) {
+        this.name = name
+        this.firstname = firstname
+        this.hiringdate = hiringdate
+        this.position = position
+        this.salary = salary
+        this.service = service
+    }
+
+numberYears(){
+
+}
